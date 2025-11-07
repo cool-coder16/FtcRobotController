e2e5c9891bff7e7da5cf37c889584868edd65ead
cool-coder16 <vivaan.loiwal@gmail.com>
2025-11-06 17:13:55 -0800

    Added Conditionals Practice Tutorial 11/06/25

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class IfPractice extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        double motorSpeed;
+
+        if (!gamepad1.a){
+            motorSpeed = gamepad1.left_stick_y * 0.5;
+        } else {
+            motorSpeed = gamepad1.left_stick_y;
+        }
+        telemetry.addData("Motor Speed", motorSpeed);
+    }
+}
+
